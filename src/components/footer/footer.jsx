cb43b79dbb7ce2af5f62109fd632ae2b854c7826
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-03-31 12:54:09 -0300

    arreglo iconos

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import "./footer.css";
-
-
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 function footer() {
-
-
-    return (
-
-        <footer className="footer">
-
-            <div className="footer-container">
-
-                <nav className="footer-menu">
-                    <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><ion-icon name="logo-facebook" size="large"></ion-icon></a>
-                    <a href="https://twitter.com/?lang=es" target="_blank" rel="noreferrer"><ion-icon name="logo-twitter" size="large"></ion-icon></a>
-                    <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><ion-icon name="logo-instagram" size="large"></ion-icon></a>
-                </nav>
-
-            </div>
-
-        </footer>
-
-    );
-
-};
-
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <nav className="footer-menu">
+          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+            <FaFacebook />
+          </a>
+          <a
+            href="https://twitter.com/?lang=es"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaTwitter />
+          </a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+            <FaInstagram />
+          </a>
+        </nav>
+      </div>
+    </footer>
+  );
+}
 
 export default footer;
